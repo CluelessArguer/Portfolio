135a32f154dcdf49da532bea317d44d8d7ce7a1f
Marc Mansour <webdev.design.code@gmail.com>
2025-01-10 17:40:31 -0500

    Experience component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import HeaderComponent from "./Components/Header/HeaderComponent";
 import PortfolioWrapperComponent from './Components/PortfolioWrapperComponent';
+import ExperienceComponent from "./Components/Experience/ExperienceComponent";
 
 function App() {
   return (
     <PortfolioWrapperComponent>
       <HeaderComponent/>
+      <ExperienceComponent/>
     </PortfolioWrapperComponent>
   );
 }
---
+++ src/Components/Experience/ExperienceComponent.jsx
@@ -0,0 +1,11 @@
+import styles from "./Experience.module.css";
+
+const ExperienceComponent=()=>{
+    return(
+        <div className={styles.experience}>
+            <h2 className={styles.experience_title}>02. Experience</h2>
+        </div>
+    )
+}
+
+export default ExperienceComponent;
