6a69db3bb5d5e82a1eaa8580d5567df31f7f1e23
Marc Mansour <webdev.design.code@gmail.com>
2025-01-10 16:20:08 -0500

    responsive design

--- src/Components/Header/NavigationComponent.jsx
+++ src/Components/Header/NavigationComponent.jsx
@@ -2,15 +2,22 @@ import styles from "./Header.module.css";
 import LinkComponent from "./LinkComponent.jsx";
 import { navLinks } from "../../constants.js";
 import ColorPickerComponent from "./ColorPickerComponent.jsx";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const NavigationComponent=()=>{
-
+    const [menuOn, setMenuOn]=useState(0);
+    const setHamburgerMenuOn=()=>{
+        setMenuOn(prev=>prev?0:1);
+    }
+    
     return(
         <div className={styles.nav_bar}>
             <div className={styles.nav_initials}>
                 M
             </div>
-            <div className={styles.nav_links}>
+            <div className={`${styles.nav_links}`}>
                 {
                     navLinks.map((link,index)=>{
                         return (
@@ -20,6 +27,21 @@ const NavigationComponent=()=>{
                 }
                 <ColorPickerComponent/>
             </div>
+            <FontAwesomeIcon className={styles.hamburger_menu} icon={faBars} onClick={setHamburgerMenuOn} size="2xl" />
+            {
+                menuOn?
+                <div className={`${styles.mobile_nav_links}`}>
+                    <FontAwesomeIcon className={styles.close_menu} icon={faX} size="xl" onClick={setHamburgerMenuOn} />
+                    {
+                        navLinks.map((link,index)=>{
+                            return (
+                                <LinkComponent key={index} number={link.number} title={link.name} href={link.href}/>
+                            )
+                        })
+                    }
+                    <ColorPickerComponent/>
+                </div>:""
+            }
         </div>
     )
 }
